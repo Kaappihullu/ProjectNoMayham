99259bc6a2f9dc32a8178448f4fc3104c618ce40
Joni Salmela <hyva_hyypio@hotmail.com>
2015-09-19 15:36:56 +0300

    VS integration sry about that

---
+++ ProjectNoMayham/Assets/EmailBot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmailBot : MonoBehaviour 
+{
+
+    public float SendInterval = 10f;
+    public float SkipSendChance = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
